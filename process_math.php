b6f2565ac844f2483410a29cc86c839b71950b67
secretlabrat <secretlaboratory333@gmail.com>
2025-02-13 15:28:39 -0800

    fix * might be wrong

--- process_math.php
+++ process_math.php
@@ -2,7 +2,7 @@
 $first = $_POST['first'];
 $second = $_POST['second'];
 $operation = $_POST['operation'];
-$command = escapeshellcmd("python3 math_operations.py $first $second $operation");
+$command = escapeshellcmd("python3 math_operations.py $first $second \"$operation\"");
 $output = shell_exec($command);
 echo $output;
 ?>
